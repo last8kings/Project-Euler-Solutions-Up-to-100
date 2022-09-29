5cf4ea3d5e3e40e65cbad4335f90aa6c88cbd598
last8kings <97848034+last8kings@users.noreply.github.com>
2022-09-29 02:00:47 -0400

    Create Problem 23 - Non-Abundant Sums.cpp

---
+++ Problem 23 - Non-Abundant Sums.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void d() { cout << "DEBUG\n";}
+
+int main() {
+
+    vector<int> SumOfDivisors(28124, 0);
+
+    for (int i = 1; i < 28124; ++i) {
+        for (int j = i * 2; j < 28124; j += i) {
+            SumOfDivisors[j] += i;
+        }
+    }
+    
+    vector<int> AbundantNums;
+    for (int i = 1; i < 28124; ++i) {
+        if (SumOfDivisors[i] > i) AbundantNums.emplace_back(i);
+    }
+    
+    vector<bool> IsSumOfTwoAbundant(28124, false);
+    
+    
+    for (int x : AbundantNums) {
+        for (int y : AbundantNums) 
+            if (x + y < 28124) IsSumOfTwoAbundant[x + y] = true;
+    }
+    
+    int Sum = 0;
+    for (int i = 1; i <= 28123; ++i) if (!IsSumOfTwoAbundant[i]) Sum += i;
+
+    cout << Sum << '\n';
+} 
