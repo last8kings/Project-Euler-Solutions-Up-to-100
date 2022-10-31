6d2ae088bab215d2886b4dd2033757a523e7dda1
last8kings <97848034+last8kings@users.noreply.github.com>
2022-10-31 00:27:27 -0400

    Update Problem 6 - Sum square difference.cpp

--- Problem 6 - Sum square difference.cpp
+++ Problem 6 - Sum square difference.cpp
@@ -1,15 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-    
 
+/*
+Instead of the for loop in my program, this can alternatively be calculated using the formula 
+1 ^ 2 + 2 ^ 2 + ... + n ^ 2 = n * (n + 1) * (n + 2) / 6 
+for the sum of squares
+and 
+(1 + 2 + ... + n) ^ 2 = (n * (n + 1) / 2) ^ 2 
+for the square of sums
+which can both be proved with induction. 
+*/
 int main() {
    int sumofsquares = 0;
    int squareofsums = 0;
-   for(int i = 1;i<=100;i++){
-      sumofsquares+=i*i;
-      squareofsums+=i;
+   for (int i = 1; i <= 100; ++i) {
+      sumofsquares += i * i;
+      squareofsums += i;
    }
    squareofsums *= squareofsums;
-   cout<<squareofsums-sumofsquares;
+   cout << squareofsums - sumofsquares;
    return 0;
-   } 
+} 
